d63150c3b51346b383d20eab96453aa1a7ce72cd
Japhy Bartlett <japhy@pearachute.com>
2025-12-31 15:37:07 -0500

    Add database.py with Peewee connection setup

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+import os
+from peewee import SqliteDatabase
+
+DATABASE_PATH = os.environ.get("DATABASE_PATH", "kanban.db")
+
+db = SqliteDatabase(DATABASE_PATH)
+
+
+def init_db():
+    db.connect()
+    from backend.models import User, Board, Column, Card
+
+    db.create_tables([User, Board, Column, Card])
+    db.close()
