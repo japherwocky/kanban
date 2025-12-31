2aa237c4986285fad566b3daee7332560af81cb1
Japhy Bartlett <japhy@pearachute.com>
2025-12-31 17:49:22 -0500

    Use bcrypt>=4.0.0 and remove warning suppression

--- backend/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import warnings
-import os
-
-warnings.filterwarnings("ignore", message=".*trapped.*error reading bcrypt version")
-warnings.filterwarnings("ignore", category=DeprecationWarning, module="passlib")
